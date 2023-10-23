58e5fa539802ed68047feae742ee1ce824684d8e
iverly <github@iverly.net>
2023-10-23 11:55:14 +0200

    feat: initial commit
    Signed-off-by: iverly <github@iverly.net>

---
+++ theme.config.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <strong>Faast</strong>,
+  docsRepositoryBase: "https://github.com/faast-rt/docs",
+  project: {
+    link: "https://github.com/faast-rt",
+  },
+};
+
+export default config;
