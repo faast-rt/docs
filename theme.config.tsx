7aea3e284b32e7dc09bb422eacef9388c6417970
iverly <github@iverly.net>
2023-10-24 11:16:30 +0200

    fix(config): docsRepositoryBase link
    Signed-off-by: iverly <github@iverly.net>

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const config: DocsThemeConfig = {
   logo: <strong>Faast</strong>,
-  docsRepositoryBase: 'https://github.com/faast-rt/docs',
+  docsRepositoryBase: 'https://github.com/faast-rt/docs/tree/main/',
   project: {
     link: 'https://github.com/faast-rt',
   },
